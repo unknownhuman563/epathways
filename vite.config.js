26b050352f0eb80cad239b478ca142f9cbb5d554
Neil E. <152400890+unknownhuman563@users.noreply.github.com>
2026-02-11 20:06:52 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    optimizeDeps: {
+        include: ["swiper"],
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        react(),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            "@": "/resources/js",
+            "@assets": "/resources/Assets",
+            "@AdminLayout": "/resources/js/components/AdminLayout"
+        }
+    }
+});
